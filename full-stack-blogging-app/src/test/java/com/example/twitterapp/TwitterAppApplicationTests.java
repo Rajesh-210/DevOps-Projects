1b9183ec510cbe55e9d9285e7e8034fa7c6c6229
Rajesh-210 <rajeshchilukuri210@gmail.com>
2026-06-10 15:50:24 +0530

    Update TwitterAppApplicationTests.java

--- full-stack-blogging-app/src/test/java/com/example/twitterapp/TwitterAppApplicationTests.java
+++ full-stack-blogging-app/src/test/java/com/example/twitterapp/TwitterAppApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.twitterapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "management.metrics.binders.processor.enabled=false"
+})
 class TwitterAppApplicationTests {
 
 	@Test
